3dd95a97d9fb211376422eba446672e5f6f8531e
Steve Armstrong <steve@horsefire.com>
2012-07-29 15:31:43 -0700

    Fixing up command-line args a bit

---
+++ src/main/java/com/horsefire/tiddly/local/CommandLineArgs.java
@@ -0,0 +1,40 @@
+package com.horsefire.tiddly.local;
+
+import java.util.List;
+
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.internal.Lists;
+
+public class CommandLineArgs {
+
+	@Parameter(names = { "--help" }, description = "Display help")
+	private boolean help;
+
+	@Parameter(names = { "--port" }, description = "Http port")
+	private final int port = 9801;
+
+	@Parameter(names = { "--logfile" }, description = "Logback config file")
+	private String logback;
+
+	@Parameter(description = "/path/to/wiki.html")
+	private final List<String> args = Lists.newArrayList();
+
+	public boolean isHelp() {
+		return help;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public String getLogback() {
+		return logback;
+	}
+
+	public String getFile() {
+		if (args.size() > 0) {
+			return args.get(0);
+		}
+		return null;
+	}
+}
--- src/main/java/com/horsefire/tiddly/local/TiddlyLocal.java
+++ src/main/java/com/horsefire/tiddly/local/TiddlyLocal.java
@@ -6,35 +6,71 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.ParameterException;
+
 public class TiddlyLocal {
 
 	public static final String LOCAL_URL = "/local/";
 
+	private static void displayHelp() {
+		StringBuilder buffer = new StringBuilder();
+		new JCommander(new CommandLineArgs()).usage(buffer);
+		System.err.println(buffer.toString());
+	}
+
 	public static void main(String[] args) throws Exception {
-		if (args.length != 1) {
-			System.err.println("Must specify a path to a file");
+		CommandLineArgs options = new CommandLineArgs();
+		try {
+			new JCommander(options, args);
+		} catch (ParameterException e) {
+			System.err.println(e.getMessage());
+			displayHelp();
 			return;
 		}
-		File file = new File(args[0]);
-		if (!file.isFile()) {
-			System.err.println("File " + file + " must exist");
+
+		if (options.isHelp()) {
+			displayHelp();
+			return;
+		}
+
+		final String logProperty = "logback.configurationFile";
+		if (options.getLogback() == null) {
+			System.setProperty(logProperty, "logback.xml");
+		} else {
+			System.setProperty(logProperty, options.getLogback());
+		}
+
+		File wikiFile;
+		if (options.getFile() == null) {
+			wikiFile = new File("wiki.html");
+			System.out
+					.println("No wiki file specified, so defaulting to wiki.html");
+		} else {
+			wikiFile = new File(options.getFile());
+		}
+		if (!wikiFile.isFile()) {
+			System.err.println("File " + wikiFile + " must exist");
 			return;
 		}
 
 		ServletContextHandler contextHandler = new ServletContextHandler();
 		contextHandler.setContextPath("/");
-		contextHandler.addServlet(new ServletHolder(new LocalServlet(file,
+		contextHandler.addServlet(new ServletHolder(new LocalServlet(wikiFile,
 				new WikiMorpher(LOCAL_URL + "wiki.html", "/tiddlybox.js"))),
 				LOCAL_URL + "wiki.html");
 		contextHandler.addServlet(new ServletHolder(new JavascriptServlet()),
 				"/tiddlybox.js");
 		contextHandler.addServlet(
-				new ServletHolder(new FileServlet(file.getParentFile())),
+				new ServletHolder(new FileServlet(wikiFile.getParentFile())),
 				LOCAL_URL + "*");
 
-		Server server = new Server(9801);
+		Server server = new Server(options.getPort());
 		server.setHandler(contextHandler);
 		server.start();
+		org.slf4j.LoggerFactory.getLogger(TiddlyLocal.class).info(
+				"Serving file at http://localhost:{}/{}wiki.html",
+				options.getPort(), LOCAL_URL);
 		server.join();
 	}
 }
